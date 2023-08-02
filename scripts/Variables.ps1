c1fc7f5a9c7b8c49a3eebc5654599eef671297e1
Mollie Munoz <mollie.munoz@microsoft.com>
2023-08-02 14:57:43 -0700

    Update local deployment scripts and main readme for quickstart (#94)
    ### Motivation and Context
    
      1. Why is this change required?  To make onboarding smoother.
    2. What problem does it solve? The quick-start local deployment steps
    were difficult to follow.
    3. What scenario does it contribute to? Anyone trying out Chat Copilot
    locally for the first time.
      4. If it fixes an open issue, please link to the issue here. NA.
    
    ### Description
    
    Main Chat Copilot Readme is updated to present a quick-start beginner
    use case. This is the local deployment of Chat Copilot, not an Azure
    deployment. It removes the redirect for the scripts, and formats
    instructions to improve readability.
    
    Bash and PowerShell scripts are also fixed to use environment variable
    `ASPNETCORE_ENVIRONMENT`, which is necessary for appsettings.json
    override (by appsettings.Development.json) when running the backend for
    OpenAI.
    
    ### Contribution Checklist
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Mollie Munoz <momuno@microsoft.com>
    Co-authored-by: Gil LaHaye <gillahaye@microsoft.com>

---
+++ scripts/Variables.ps1
@@ -0,0 +1,14 @@
+# Default environment file to be read by scripts
+
+# Default values
+$varCompletionModelOpenAI="gpt-3.5-turbo"
+$varPlannerModelOpenAI="gpt-3.5-turbo"
+$varCompletionModelAzureOpenAI="gpt-35-turbo"
+$varPlannerModelAzureOpenAI="gpt-35-turbo"
+$varEmbeddingModel="text-embedding-ada-002"
+$varTenantId="common"
+$varASPNetCore="Development"
+
+# Constants
+$varAzureOpenAI="AzureOpenAI"
+$varOpenAI="OpenAI"
