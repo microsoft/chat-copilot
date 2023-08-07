cb727007e3b989595ab6fe2c71235e995bc62bd2
Teresa Hoang <tehoang@microsoft.com>
2023-08-07 11:35:44 -0700

    🛠️ Refactor StepwiseStepView and StepwiseThoughtProcess component

--- webapp/src/components/chat/prompt-dialog/stepwise-planner/StepwiseStepView.tsx
+++ webapp/src/components/chat/prompt-dialog/stepwise-planner/StepwiseStepView.tsx
@@ -1,49 +1,88 @@
-import { AccordionHeader, AccordionItem, AccordionPanel, Body1, Body2 } from '@fluentui/react-components';
+import {
+    AccordionHeader,
+    AccordionItem,
+    AccordionPanel,
+    Body1,
+    makeStyles,
+    shorthands,
+    tokens,
+} from '@fluentui/react-components';
 import { StepwiseStep } from '../../../../libs/models/StepwiseStep';
+
+const useClasses = makeStyles({
+    root: {
+        display: 'flex',
+        ...shorthands.gap(tokens.spacingHorizontalM),
+    },
+    accordionItem: {
+        width: '100%',
+    },
+    header: {
+        width: '100%',
+        /* Styles for the button within the header */
+        '& button': {
+            alignItems: 'flex-start',
+            minHeight: '-webkit-fill-available',
+            paddingLeft: tokens.spacingHorizontalNone,
+        },
+    },
+});
 
 interface IStepwiseStepViewProps {
     step: StepwiseStep;
+    index: number;
 }
 
-export const StepwiseStepView: React.FC<IStepwiseStepViewProps> = ({ step }) => {
+export const StepwiseStepView: React.FC<IStepwiseStepViewProps> = ({ step, index }) => {
+    const classes = useClasses();
+
     let header = `[OBSERVATION] ${step.observation}`;
     let details: string | undefined;
 
     if (step.action) {
         header = `[ACTION] ${step.action}`;
         details = step.action_variables
-            ? Object.entries(step.action_variables)
+            ? 'Action variables: \n' +
+              Object.entries(step.action_variables)
                   .map(([key, value]) => `${key}: ${value}`)
                   .join('\n')
             : '';
-
         const observation = step.observation?.replace(/[\\r]/g, '').replace(/\\{1,2}u0022/g, '"');
-        details = details.concat(`\nObservation: ${observation}`);
+        details = details.concat(`\nObservation: \n${observation}`);
     }
 
     if (step.thought) {
-        const thoughtRegEx = /\[(THOUGHT|QUESTION|ACTION)\]\s*(.*)$/g;
-        header = step.thought.match(thoughtRegEx)?.[0].replace(/\\n/g, ' ') ?? step.thought;
+        const thoughtRegEx = /\[(THOUGHT|QUESTION|ACTION)]\s*(.*)/g;
+        let thought = step.thought.match(thoughtRegEx)?.[0].replace(/\\n/g, ' ') ?? `[THOUGHT] ${step.thought}`;
+        const sentences = thought.split('. ');
+        if (sentences.length > 1) {
+            thought = sentences.length > 1 ? sentences[0] + '.' : thought;
+            details = sentences.slice(1).join('. ');
+        }
+        header = thought;
     }
 
     return (
-        <AccordionItem value={header}>
-            {step.action ? (
-                <>
-                    <AccordionHeader expandIconPosition="end">
-                        <Body2>{header}</Body2>
-                    </AccordionHeader>
-                    <AccordionPanel>
-                        <Body1>
-                            {details
-                                ?.split('\n')
-                                .map((paragraph, idx) => <p key={`step-details-${idx}`}>{paragraph}</p>)}
-                        </Body1>
-                    </AccordionPanel>
-                </>
-            ) : (
-                <Body2>{header}</Body2>
-            )}
-        </AccordionItem>
+        <div className={classes.root}>
+            <Body1>{index + 1}.</Body1>
+            <AccordionItem value={index} className={classes.accordionItem}>
+                {details ? (
+                    <>
+                        <AccordionHeader expandIconPosition="end" className={classes.header}>
+                            <Body1>{header}</Body1>
+                        </AccordionHeader>
+                        <AccordionPanel>
+                            <Body1>
+                                {details.split('\n').map((paragraph, idx) => (
+                                    <p key={`step-details-${idx}`}>{paragraph}</p>
+                                ))}
+                            </Body1>
+                        </AccordionPanel>
+                    </>
+                ) : (
+                    <Body1>{header}</Body1>
+                )}
+            </AccordionItem>
+        </div>
     );
 };
--- webapp/src/components/chat/prompt-dialog/stepwise-planner/StepwiseThoughtProcess.tsx
+++ webapp/src/components/chat/prompt-dialog/stepwise-planner/StepwiseThoughtProcess.tsx
@@ -1,23 +1,36 @@
-import { Accordion, Body2 } from '@fluentui/react-components';
+import { Accordion, Body1, makeStyles, mergeClasses, shorthands, tokens } from '@fluentui/react-components';
 import { StepwiseStep } from '../../../../libs/models/StepwiseStep';
 import { StepwiseStepView } from './StepwiseStepView';
+
+const useClasses = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        ...shorthands.gap(tokens.spacingHorizontalSNudge),
+    },
+    header: {
+        paddingTop: tokens.spacingVerticalS,
+    },
+});
 
 interface IStepwiseThoughtProcessProps {
     stepwiseResult: string;
 }
 
 export const StepwiseThoughtProcess: React.FC<IStepwiseThoughtProcessProps> = ({ stepwiseResult }) => {
+    const classes = useClasses();
+
     const thoughtProcessRegEx = /Result not found, review _stepsTaken to see what happened\.\s+(\[{.*}])/g;
     const matches = stepwiseResult.matchAll(thoughtProcessRegEx);
     const match = Array.from(matches);
     if (match.length > 0) {
         const steps = JSON.parse(match[0][1]) as StepwiseStep[];
         return (
-            <div>
-                <Body2>[THOUGHT PROCESS]</Body2>
-                <Accordion collapsible multiple>
+            <div className={mergeClasses(classes.root, classes.header)}>
+                <Body1>[THOUGHT PROCESS]</Body1>
+                <Accordion collapsible multiple className={classes.root}>
                     {steps.map((step, index) => {
-                        return <StepwiseStepView step={step} key={`stepwise-thought-${index}`} />;
+                        return <StepwiseStepView step={step} key={`stepwise-thought-${index}`} index={index} />;
                     })}
                 </Accordion>
             </div>
