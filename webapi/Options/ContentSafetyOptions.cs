ec746e84aa3d95b4bc6e016ae1c57987c138eccb
Chris <66376200+crickman@users.noreply.github.com>
2023-08-21 14:12:34 -0700

    Tweak ContentSafetyOptions.Enabled attribution and stream-line ServiceExtensiosn (#234)
    ### Motivation and Context
    
    ```
    Unhandled exception. Microsoft.Extensions.Options.OptionsValidationException: DataAnnotation validation failed for 'ContentSafetyOptions' members: '' with the error: ''Enabled' must be a string.'.
       at Microsoft.Extensions.Options.OptionsFactory`1.Create(String name)
       at System.Lazy`1.ViaFactory(LazyThreadSafetyMode mode)
       at System.Lazy`1.ExecutionAndPublication(LazyHelper executionAndPublication, Boolean useDefaultConstructor)
       at System.Lazy`1.CreateValue()
       at Microsoft.Extensions.Options.OptionsCache`1.GetOrAdd[TArg](String name, Func`3 createOptions, TArg factoryArgument)
       at Microsoft.Extensions.Options.OptionsMonitor`1.Get(String name)
       at Microsoft.Extensions.DependencyInjection.OptionsBuilderExtensions.<>c__DisplayClass0_1`1.<ValidateOnStart>b__1()
       at Microsoft.Extensions.DependencyInjection.ValidationHostedService.StartAsync(CancellationToken cancellationToken)
    --- End of stack trace from previous location ---
       at Microsoft.Extensions.DependencyInjection.ValidationHostedService.StartAsync(CancellationToken cancellationToken)
       at Microsoft.Extensions.Hosting.Internal.Host.StartAsync(CancellationToken cancellationToken)
       at Microsoft.Extensions.Hosting.HostingAbstractionsHostExtensions.RunAsync(IHost host, CancellationToken token)
       at Microsoft.Extensions.Hosting.HostingAbstractionsHostExtensions.RunAsync(IHost host, CancellationToken token)
       at CopilotChat.WebApi.Program.Main(String[] args) in C:\Users\crickman\source\repos\sk-chat-copilot\webapi\Program.cs:line 115
       at CopilotChat.WebApi.Program.<Main>(String[] args)
    ```
    <!-- Thank you for your contribution to the copilot-chat repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    - I think the whitespace instruction confused the validation on the
    type.
    - Reducing repeated code / increase consistency in
    `ServiceExtensions.cs`
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [Contribution
    Guidelines](https://github.com/microsoft/copilot-chat/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/copilot-chat/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- webapi/Options/ContentSafetyOptions.cs
+++ webapi/Options/ContentSafetyOptions.cs
@@ -15,7 +15,7 @@ public class ContentSafetyOptions
     /// <summary>
     /// Whether to enable content safety.
     /// </summary>
-    [Required, NotEmptyOrWhitespace]
+    [Required]
     public bool Enabled { get; set; } = false;
 
     /// <summary>
