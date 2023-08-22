53b81db4a2f7c5d7462ec17117cbe94a32505119
Teresa Hoang <tehoang@microsoft.com>
2023-08-22 10:56:51 -0700

    Changing string

--- webapp/src/assets/strings.ts
+++ webapp/src/assets/strings.ts
@@ -3,6 +3,6 @@ export const COPY = {
     CHAT_DELETED_MESSAGE: (chatName?: string) =>
         `Chat ${
             chatName ? `{${chatName}} ` : ''
-        }has been deleted by another user. Please save any resources you need and refresh the page.`,
+        }has been removed by another user. You can still access the latest chat history for now. All chat content will be cleared once you refresh or exit the application.`,
     REFRESH_APP_ADVISORY: 'Please refresh the page to ensure you have the latest data.',
 };
