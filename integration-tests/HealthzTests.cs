ce5a0a104dff2d2321295ee66c6e7b686c764bf3
Gil LaHaye <gillahaye@microsoft.com>
2023-08-29 12:50:41 -0400

    Add integration test project (#268)
    ### Motivation and Context
    We want to make our CI/CD pipeline more robust
    
    ### Description
    Added an integration test project.
    
    ### Contribution Checklist
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [Contribution
    Guidelines](https://github.com/microsoft/copilot-chat/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/copilot-chat/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ integration-tests/HealthzTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http;
+using Xunit;
+
+namespace ChatCopilotIntegrationTests;
+
+/// <summary>
+/// Class for testing the healthcheck endpoint
+/// </summary>
+public class HealthzTests : ChatCopilotIntegrationTest
+{
+    [Fact]
+    public async void HealthzSuccessfullyReturns()
+    {
+        HttpResponseMessage response = await this._httpClient.GetAsync("healthz");
+
+        response.EnsureSuccessStatusCode();
+    }
+}
