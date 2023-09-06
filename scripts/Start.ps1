32cab77393fd28c940226f875a5fc27cfd833681
Mark McFadden <m2web@yahoo.com>
2023-09-06 08:00:39 -0400

    feat: :zap: Update Start.ps1 to check that backend is running before starting frontend.

--- scripts/Start.ps1
+++ scripts/Start.ps1
@@ -8,6 +8,12 @@ $FrontendScript = Join-Path "$PSScriptRoot" 'Start-Frontend.ps1'
 
 # Start backend (in new PS process)
 Start-Process pwsh -ArgumentList "-command $BackendScript"
+#check if the backend is running before proceeding
+$backendRunning = $false
+while ($backendRunning -eq $false) {
+  $backendRunning = Test-NetConnection -ComputerName localhost -Port 40443 -InformationLevel Quiet
+  Start-Sleep -Seconds 5
+}
 
 # Start frontend (in current PS process)
 & $FrontendScript
