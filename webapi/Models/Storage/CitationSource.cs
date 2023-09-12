c45639e7f359aa2458f8db9ae22a394a185fa871
Tao Chen <taochen@microsoft.com>
2023-09-12 10:31:37 -0700

    Add snippet and relevance score to citation source

--- webapi/Models/Storage/CitationSource.cs
+++ webapi/Models/Storage/CitationSource.cs
@@ -27,15 +27,27 @@ public class CitationSource
     public string SourceName { get; set; } = string.Empty;
 
     /// <summary>
+    /// The snippet of the citation.
+    /// </summary>
+    public string Snippet { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Relevance score of the citation against the query.
+    /// </summary>
+    public double RelevanceScore { get; set; } = 0.0;
+
+    /// <summary>
     /// Converts a <see cref="Citation"/> to a <see cref="CitationSource"/>.
     /// </summary>
-    public static CitationSource FromSemanticMemoryCitation(Citation citation)
+    public static CitationSource FromSemanticMemoryCitation(Citation citation, string snippet, double relevanceScore)
     {
         var citationSource = new CitationSource
         {
             Link = citation.Link,
             SourceContentType = citation.SourceContentType,
             SourceName = citation.SourceName,
+            Snippet = snippet,
+            RelevanceScore = relevanceScore
         };
 
         return citationSource;
