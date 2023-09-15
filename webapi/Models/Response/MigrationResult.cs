d3e36b15b1d9f3fa84c9f18b08e4c5da5e912561
Chris Rickman <crickman@microsoft.com>
2023-09-15 09:23:42 -0700

    Comments

--- webapi/Models/Response/MigrationResult.cs
+++ webapi/Models/Response/MigrationResult.cs
@@ -3,22 +3,31 @@
 namespace CopilotChat.WebApi.Models.Response;
 
 /// <summary>
-/// $$$
+/// Defines optional messaging for maintenace mode.
 /// </summary>
-public class MigrationResult
+public class MaintenanceResult
 {
     /// <summary>
-    /// $$$
+    /// The maintenace notification title.
     /// </summary>
+    /// <remarks>
+    /// Will utilize default if not defined.
+    /// </remarks>
     public string Title { get; set; } = string.Empty;
 
     /// <summary>
-    /// $$$
+    /// The maintenace notification message.
     /// </summary>
+    /// <remarks>
+    /// Will utilize default if not defined.
+    /// </remarks>
     public string Message { get; set; } = string.Empty;
 
     /// <summary>
-    /// $$$
+    /// The maintenace notification note.
     /// </summary>
+    /// <remarks>
+    /// Will utilize default if not defined.
+    /// </remarks>
     public string? Note { get; set; }
 }
