2291d09f10445322c7c9aa760963ffdc4ace7680
MaggieWang0 <yingwa@microsoft.com>
2023-09-22 17:44:31 +0800

    Fix issue 378 to able to use scripts with spaces in full path

--- scripts/Start.ps1
+++ scripts/Start.ps1
@@ -3,11 +3,10 @@
 Initializes and runs both the backend and frontend for Chat Copilot.
 #>
 
-$BackendScript = Join-Path "$PSScriptRoot" 'Start-Backend.ps1'
 $FrontendScript = Join-Path "$PSScriptRoot" 'Start-Frontend.ps1'
 
 # Start backend (in new PS process)
-Start-Process pwsh -ArgumentList "-command $BackendScript"
+Start-Process powershell -WorkingDirectory "$PSScriptRoot" -ArgumentList '-NoExit -Command ".\`"Start-Backend.ps1""'
 # check if the backend is running before proceeding
 $backendRunning = $false
 
