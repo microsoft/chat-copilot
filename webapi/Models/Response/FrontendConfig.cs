80b6be4b1229bfa11a11d73e923d5d5e5fc67efc
Gil LaHaye <gillahaye@microsoft.com>
2023-09-27 12:50:29 -0700

    Serve static files and frontend config from backend

---
+++ webapi/Models/Response/FrontendConfig.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace CopilotChat.WebApi.Models.Response;
+
+/// <summary>
+/// Configuration to be used by the frontend client to this service.
+/// </summary>
+public class FrontendConfig
+{
+    /// <summary>
+    /// Type of auth to use.
+    /// </summary>
+    [JsonPropertyName("authType")]
+    public string AuthType { get; set; } = "AzureAd";
+
+    /// <summary>
+    /// Azure Active Directory authority to use.
+    /// </summary>
+    [JsonPropertyName("aadAuthority")]
+    public string AadAuthority { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Azure Active Directory client ID the frontend is to use.
+    /// </summary>
+    [JsonPropertyName("aadClient")]
+    public string AadClient { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Azure Active Directory scope the frontend should request.
+    /// </summary>
+    [JsonPropertyName("aadApiScope")]
+    public string AadApiScope { get; set; } = string.Empty;
+}
