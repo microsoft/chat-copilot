e7f333b41cf63a67f514776f47ffae0c05b7f1f2
Chris <66376200+crickman@users.noreply.github.com>
2023-11-08 11:51:15 -0800

    Fix Document/Memory Removal when Chat Deleted (#585)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the chat-copilot repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    Customer reported issue.  Thank you!
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    - Extract documentId from partition link
    - Parallelize tasks
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [Contribution
    Guidelines](https://github.com/microsoft/chat-copilot/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/chat-copilot/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- webapi/Extensions/ISemanticMemoryClientExtensions.cs
+++ webapi/Extensions/ISemanticMemoryClientExtensions.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CopilotChat.Shared;
@@ -182,9 +183,9 @@ internal static class ISemanticMemoryClientExtensions
         CancellationToken cancellationToken = default)
     {
         var memories = await memoryClient.SearchMemoryAsync(indexName, "*", 0.0F, chatId, cancellationToken: cancellationToken);
-        foreach (var memory in memories.Results)
-        {
-            await memoryClient.DeleteDocumentAsync(indexName, memory.Link, cancellationToken);
-        }
+        var documentIds = memories.Results.Select(memory => memory.Link.Split('/').First()).Distinct().ToArray();
+        var tasks = documentIds.Select(documentId => memoryClient.DeleteDocumentAsync(documentId, indexName, cancellationToken)).ToArray();
+
+        Task.WaitAll(tasks, cancellationToken);
     }
 }
