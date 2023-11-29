af43a150bca1f493832a86c24037120669f43aa0
Gil LaHaye <gillahaye@microsoft.com>
2023-11-29 08:03:15 -0800

    Add integration tests (#651)
    ### Motivation and Context
    Higher automated test coverage is desired.
    
    ### Description
    - Make integration tests able to auth with a CC server
    - Add int tests for the service info and speech token controllers
    
    ### Contribution Checklist
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [Contribution
    Guidelines](https://github.com/microsoft/chat-copilot/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/chat-copilot/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Mollie Munoz <mollie.munoz@microsoft.com>

--- integration-tests/ChatCopilotIntegrationTest.cs
+++ integration-tests/ChatCopilotIntegrationTest.cs
@@ -1,8 +1,12 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
 using Xunit;
 
 namespace ChatCopilotIntegrationTests;
@@ -10,23 +14,30 @@ namespace ChatCopilotIntegrationTests;
 /// <summary>
 /// Base class for Chat Copilot integration tests
 /// </summary>
+[Trait("Category", "Integration Tests")]
 public abstract class ChatCopilotIntegrationTest : IDisposable
 {
-    protected const string BaseUrlSettingName = "BaseUrl";
+    protected const string BaseUrlSettingName = "BaseServerUrl";
+    protected const string ClientIdSettingName = "ClientID";
+    protected const string AuthoritySettingName = "Authority";
+    protected const string UsernameSettingName = "Username";
+    protected const string PasswordSettingName = "Password";
+    protected const string ScopesSettingName = "Scopes";
 
     protected readonly HttpClient _httpClient;
+    protected readonly IConfigurationRoot configuration;
 
     protected ChatCopilotIntegrationTest()
     {
         // Load configuration
-        IConfigurationRoot configuration = new ConfigurationBuilder()
+        this.configuration = new ConfigurationBuilder()
             .AddJsonFile(path: "testsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
             .AddEnvironmentVariables()
             .AddUserSecrets<HealthzTests>()
             .Build();
 
-        string? baseUrl = configuration[BaseUrlSettingName];
+        string? baseUrl = this.configuration[BaseUrlSettingName];
         Assert.False(string.IsNullOrEmpty(baseUrl));
         Assert.True(baseUrl.EndsWith('/'));
 
@@ -46,5 +57,40 @@ public abstract class ChatCopilotIntegrationTest : IDisposable
         {
             this._httpClient.Dispose();
         }
+    }
+
+    protected async Task SetUpAuth()
+    {
+        string accesstoken = await this.GetUserTokenByPassword();
+        Assert.True(!string.IsNullOrEmpty(accesstoken));
+
+        this._httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accesstoken);
+    }
+
+    protected async Task<string> GetUserTokenByPassword()
+    {
+        IPublicClientApplication app = PublicClientApplicationBuilder.Create(this.configuration[ClientIdSettingName])
+                                                                     .WithAuthority(this.configuration[AuthoritySettingName])
+                                                                     .Build();
+
+        string? scopeString = this.configuration[ScopesSettingName];
+        Assert.NotNull(scopeString);
+
+        string[] scopes = scopeString.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        var accounts = await app.GetAccountsAsync();
+
+        AuthenticationResult? result = null;
+
+        if (accounts.Any())
+        {
+            result = await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault()).ExecuteAsync();
+        }
+        else
+        {
+            result = await app.AcquireTokenByUsernamePassword(scopes, this.configuration[UsernameSettingName], this.configuration[PasswordSettingName]).ExecuteAsync();
+        }
+
+        return result?.AccessToken ?? string.Empty;
     }
 }
---
+++ integration-tests/SpeechTokenTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http;
+using System.Text.Json;
+using CopilotChat.WebApi.Models.Response;
+using Xunit;
+
+namespace ChatCopilotIntegrationTests;
+
+public class SpeechTokenTests : ChatCopilotIntegrationTest
+{
+    [Fact]
+    public async void GetSpeechToken()
+    {
+        await this.SetUpAuth();
+
+        HttpResponseMessage response = await this._httpClient.GetAsync("speechToken/");
+        response.EnsureSuccessStatusCode();
+
+        var contentStream = await response.Content.ReadAsStreamAsync();
+        var speechTokenResponse = await JsonSerializer.DeserializeAsync<SpeechTokenResponse>(contentStream, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        Assert.NotNull(speechTokenResponse);
+        Assert.True((speechTokenResponse.IsSuccess == true && !string.IsNullOrEmpty(speechTokenResponse.Token)) ||
+                     speechTokenResponse.IsSuccess == false);
+    }
+}
