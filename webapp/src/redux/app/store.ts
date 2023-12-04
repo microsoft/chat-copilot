b0c2d965d297d8bcf675ea1af52e9873b5c9fea2
Desmond Howard <dehoward@microsoft.com>
2023-12-04 18:52:11 -0500

    fix breaking changes in types

--- webapp/src/redux/app/store.ts
+++ webapp/src/redux/app/store.ts
@@ -1,6 +1,14 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-import { AnyAction, Dispatch, MiddlewareAPI, MiddlewareArray, ThunkMiddleware, configureStore } from '@reduxjs/toolkit';
+import {
+    Action,
+    Dispatch,
+    MiddlewareAPI,
+    ThunkMiddleware,
+    Tuple,
+    UnknownAction,
+    configureStore,
+} from '@reduxjs/toolkit';
 import { AppState } from '../features/app/AppState';
 import { ConversationsState } from '../features/conversations/ConversationsState';
 import { signalRMiddleware } from '../features/message-relay/signalRMiddleware';
@@ -10,13 +18,7 @@ import resetStateReducer, { resetApp } from './rootReducer';
 
 export type StoreMiddlewareAPI = MiddlewareAPI<Dispatch, RootState>;
 export type Store = typeof store;
-export const store = configureStore<
-    RootState,
-    AnyAction,
-    MiddlewareArray<
-        [ThunkMiddleware<RootState>, (store: StoreMiddlewareAPI) => (next: Dispatch) => (action: any) => any]
-    >
->({
+export const store = configureStore<RootState, Action, Tuple<Array<ThunkMiddleware<RootState, UnknownAction>>>>({
     reducer: resetStateReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(signalRMiddleware),
 });
