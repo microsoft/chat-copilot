4559fcec6ccd9b09543f392dce55c79e78ab16fa
Gil LaHaye <gillahaye@microsoft.com>
2024-02-28 09:39:03 -0800

    Remove remnants of memory migration (#823)
    ### Motivation and Context
    Memory migration has been removed but some of its code was still
    present.
    
    ### Description
    Remove leftover memory migration code
    
    ### Contribution Checklist
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [Contribution
    Guidelines](https://github.com/microsoft/chat-copilot/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/chat-copilot/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- webapi/Services/SemanticKernelProvider.cs
+++ webapi/Services/SemanticKernelProvider.cs
@@ -2,17 +2,11 @@
 
 using System;
 using System.Net.Http;
-using System.Threading;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.KernelMemory;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.AzureAISearch;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using Microsoft.SemanticKernel.Connectors.Qdrant;
-using Microsoft.SemanticKernel.Memory;
 
 namespace CopilotChat.WebApi.Services;
 
@@ -21,26 +15,17 @@ namespace CopilotChat.WebApi.Services;
 /// </summary>
 public sealed class SemanticKernelProvider
 {
-    private static IMemoryStore? _volatileMemoryStore;
-
     private readonly IKernelBuilder _builderChat;
-    private readonly MemoryBuilder _builderMemory;
 
     public SemanticKernelProvider(IServiceProvider serviceProvider, IConfiguration configuration, IHttpClientFactory httpClientFactory)
     {
         this._builderChat = InitializeCompletionKernel(serviceProvider, configuration, httpClientFactory);
-        this._builderMemory = InitializeMigrationMemory(serviceProvider, configuration, httpClientFactory);
     }
 
     /// <summary>
     /// Produce semantic-kernel with only completion services for chat.
     /// </summary>
     public Kernel GetCompletionKernel() => this._builderChat.Build();
-
-    /// <summary>
-    /// Produce semantic-kernel with kernel memory.
-    /// </summary>
-    public ISemanticTextMemory MigrationMemory => this._builderMemory.Build();
 
     private static IKernelBuilder InitializeCompletionKernel(
         IServiceProvider serviceProvider,
@@ -64,12 +49,10 @@ public sealed class SemanticKernelProvider
                     azureAIOptions.Endpoint,
                     azureAIOptions.APIKey,
                     httpClient: httpClientFactory.CreateClient());
-#pragma warning restore CA2000
                 break;
 
             case string x when x.Equals("OpenAI", StringComparison.OrdinalIgnoreCase):
                 var openAIOptions = memoryOptions.GetServiceConfig<OpenAIConfig>(configuration, "OpenAI");
-#pragma warning disable CA2000 // No need to dispose of HttpClient instances from IHttpClientFactory
                 builder.AddOpenAIChatCompletion(
                     openAIOptions.TextModel,
                     openAIOptions.APIKey,
@@ -82,83 +65,5 @@ public sealed class SemanticKernelProvider
         }
 
         return builder;
-    }
-
-    private static MemoryBuilder InitializeMigrationMemory(
-        IServiceProvider serviceProvider,
-        IConfiguration configuration,
-        IHttpClientFactory httpClientFactory)
-    {
-        var memoryOptions = serviceProvider.GetRequiredService<IOptions<KernelMemoryConfig>>().Value;
-
-        var builder = new MemoryBuilder();
-
-        builder.WithLoggerFactory(serviceProvider.GetRequiredService<ILoggerFactory>());
-        builder.WithMemoryStore(CreateMemoryStore());
-
-        switch (memoryOptions.Retrieval.EmbeddingGeneratorType)
-        {
-            case string x when x.Equals("AzureOpenAI", StringComparison.OrdinalIgnoreCase):
-            case string y when y.Equals("AzureOpenAIEmbedding", StringComparison.OrdinalIgnoreCase):
-                var azureAIOptions = memoryOptions.GetServiceConfig<AzureOpenAIConfig>(configuration, "AzureOpenAIEmbedding");
-#pragma warning disable CA2000 // No need to dispose of HttpClient instances from IHttpClientFactory
-                builder.WithAzureOpenAITextEmbeddingGeneration(
-                    azureAIOptions.Deployment,
-                    azureAIOptions.Endpoint,
-                    azureAIOptions.APIKey,
-                    httpClient: httpClientFactory.CreateClient());
-#pragma warning restore CA2000
-                break;
-
-            case string x when x.Equals("OpenAI", StringComparison.OrdinalIgnoreCase):
-                var openAIOptions = memoryOptions.GetServiceConfig<OpenAIConfig>(configuration, "OpenAI");
-#pragma warning disable CA2000 // No need to dispose of HttpClient instances from IHttpClientFactory
-                builder.WithOpenAITextEmbeddingGeneration(
-                    openAIOptions.EmbeddingModel,
-                    openAIOptions.APIKey,
-                    httpClient: httpClientFactory.CreateClient());
-#pragma warning restore CA2000
-                break;
-
-            default:
-                throw new ArgumentException($"Invalid {nameof(memoryOptions.Retrieval.EmbeddingGeneratorType)} value in 'KernelMemory' settings.");
-        }
-        return builder;
-
-        IMemoryStore CreateMemoryStore()
-        {
-            switch (memoryOptions.Retrieval.MemoryDbType)
-            {
-                case string x when x.Equals("SimpleVectorDb", StringComparison.OrdinalIgnoreCase):
-                    // Maintain single instance of volatile memory.
-                    Interlocked.CompareExchange(ref _volatileMemoryStore, new VolatileMemoryStore(), null);
-                    return _volatileMemoryStore;
-
-                case string x when x.Equals("Qdrant", StringComparison.OrdinalIgnoreCase):
-                    var qdrantConfig = memoryOptions.GetServiceConfig<QdrantConfig>(configuration, "Qdrant");
-
-#pragma warning disable CA2000 // Ownership passed to QdrantMemoryStore
-                    HttpClient httpClient = new(new HttpClientHandler { CheckCertificateRevocationList = true });
-#pragma warning restore CA2000 // Ownership passed to QdrantMemoryStore
-                    if (!string.IsNullOrWhiteSpace(qdrantConfig.APIKey))
-                    {
-                        httpClient.DefaultRequestHeaders.Add("api-key", qdrantConfig.APIKey);
-                    }
-
-                    return
-                        new QdrantMemoryStore(
-                            httpClient: httpClient,
-                            1536,
-                            qdrantConfig.Endpoint,
-                            loggerFactory: serviceProvider.GetRequiredService<ILoggerFactory>());
-
-                case string x when x.Equals("AzureAISearch", StringComparison.OrdinalIgnoreCase):
-                    var acsConfig = memoryOptions.GetServiceConfig<AzureAISearchConfig>(configuration, "AzureAISearch");
-                    return new AzureAISearchMemoryStore(acsConfig.Endpoint, acsConfig.APIKey);
-
-                default:
-                    throw new InvalidOperationException($"Invalid 'MemoryDbType' type '{memoryOptions.Retrieval.MemoryDbType}'.");
-            }
-        }
     }
 }
