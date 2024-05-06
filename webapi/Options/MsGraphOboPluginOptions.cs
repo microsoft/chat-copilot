01c85a485dd870255d0eebc7c6899d3210339646
Fabian Alves <falves@microsoft.com>
2024-05-06 17:00:45 -0300

    space formatting issue

--- webapi/Options/MsGraphOboPluginOptions.cs
+++ webapi/Options/MsGraphOboPluginOptions.cs
@@ -1,24 +1,24 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 
 namespace CopilotChat.WebApi.Options;
 
 public class MsGraphOboPluginOptions
 {
-    public const string PropertyName = "OnBehalfOf";
-    /// <summary>
-    /// The authority to use for OBO Auth.
-    /// </summary>
-    public string? Authority { get; set; }
-    /// <summary>
-    /// The Tenant Id to use for OBO Auth.
-    /// </summary>
-    public string? TenantId { get; set; }
-    /// <summary>
-    /// The Client Id to use for OBO Auth.
-    /// </summary>
-    public string? ClientId { get; set; }
-    /// <summary>
-    /// The Client Secret to use for OBO Auth.
-    /// </summary>
-    public string? ClientSecret { get; set; }
+  public const string PropertyName = "OnBehalfOf";
+  /// <summary>
+  /// The authority to use for OBO Auth.
+  /// </summary>
+  public string? Authority { get; set; }
+  /// <summary>
+  /// The Tenant Id to use for OBO Auth.
+  /// </summary>
+  public string? TenantId { get; set; }
+  /// <summary>
+  /// The Client Id to use for OBO Auth.
+  /// </summary>
+  public string? ClientId { get; set; }
+  /// <summary>
+  /// The Client Secret to use for OBO Auth.
+  /// </summary>
+  public string? ClientSecret { get; set; }
 }
