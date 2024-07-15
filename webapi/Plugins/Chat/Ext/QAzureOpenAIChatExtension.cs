f11128d71e62c000dfac548bd58970fb1fe7f916
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-07-15 10:48:45 -0700

    Changes to support runtime index based bot response..

---
+++ webapi/Plugins/Chat/Ext/QAzureOpenAIChatExtension.cs
@@ -0,0 +1,65 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.Logging;
+
+
+namespace CopilotChat.WebApi.Plugins.Chat.Ext;
+
+public class QAzureOpenAIChatExtension
+{
+
+    /// <summary>
+    /// A logger instance to log events.
+    /// </summary>
+    private ILogger _logger;
+
+    private QAzureOpenAIChatOptions _qAzureOpenAIChatOptions;
+
+    public QAzureOpenAIChatExtension(ILogger logger, QAzureOpenAIChatOptions qAzureOpenAIChatOptions)
+    {
+        this._logger = logger;
+        this._qAzureOpenAIChatOptions = qAzureOpenAIChatOptions;
+    }
+
+    public bool isEnabled()
+    {
+        return this._qAzureOpenAIChatOptions.Enabled;
+    }
+
+    /// <summary>
+    /// Extension method to support passing Azure Search options for chatCompletions.
+    /// </summary>
+    public AzureChatExtensionsOptions GetAzureChatExtensionsOptions()
+    {
+
+        this._logger.LogDebug("Enabling QAzureChatExtensionsOptions.");
+        AISearchChatCompletionOption? qsearchCompletionOption = this._qAzureOpenAIChatOptions.AISearchChatCompletion;
+        return new AzureChatExtensionsOptions()
+        {
+            Extensions =
+                {
+                    new AzureSearchChatExtensionConfiguration()
+                    {
+                        Filter = null,
+                        IndexName  = qsearchCompletionOption?.IndexName,
+                        SearchEndpoint=qsearchCompletionOption?.Endpoint,
+                        Strictness = qsearchCompletionOption?.Strictness,
+                        FieldMappingOptions = new AzureSearchIndexFieldMappingOptions {
+                            UrlFieldName = qsearchCompletionOption?.FieldMapping?.UrlFieldName,
+                            TitleFieldName = qsearchCompletionOption?.FieldMapping?.TitleFieldName,
+                            FilepathFieldName = qsearchCompletionOption?.FieldMapping?.FilepathFieldName,
+                        },
+                        SemanticConfiguration = qsearchCompletionOption?.SemanticConfiguration,
+                        QueryType = new AzureSearchQueryType(qsearchCompletionOption?.QueryType),
+                        ShouldRestrictResultScope = qsearchCompletionOption?.RestrictResultScope,
+                        RoleInformation = qsearchCompletionOption?.RoleInformation,
+                        DocumentCount = qsearchCompletionOption?.DocumentCount,
+                        Authentication = new OnYourDataApiKeyAuthenticationOptions (qsearchCompletionOption?.APIKey),
+                        VectorizationSource = new OnYourDataEndpointVectorizationSource (
+                           qsearchCompletionOption?.VectorizationSource?.Endpoint,
+                           new OnYourDataApiKeyAuthenticationOptions (qsearchCompletionOption?.VectorizationSource?.APIKey))
+                    }
+                }
+
+        };
+    }
+}
