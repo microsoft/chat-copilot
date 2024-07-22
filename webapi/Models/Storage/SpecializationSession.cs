c4902e5632e17309e1e69b99003605f99c29faa5
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-07-22 16:36:44 -0700

    Draft commit for specialization

---
+++ webapi/Models/Storage/SpecializationSession.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// Model for managing specialization in DB.
+/// Currently, this is defined to store only specialization metadata(Key/Name/Description/ImageUrl).
+/// NOTE: The key will be mapped with QAzureOpenAIChatOptions for LLM request settings.
+/// </summary>
+namespace CopilotChat.WebApi.Models.Storage;
+
+/// <summary>
+/// A specialization session
+/// </summary>
+public class SpecializationSession
+{
+    /// <summary>
+    /// Key that is persistent and unique.
+    /// </summary>
+    public string Key { get; set; }
+
+    /// <summary>
+    /// Name of the specialization.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Description of the specialization.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Image URL for pictorial description of specialization or logo etc.
+    /// </summary>
+    public string ImageFilepath { get; set; }
+
+    public SpecializationSession(string Key, string Name, string Description, string ImageFilepath)
+    {
+        this.Key = Key;
+        this.Name = Name;
+        this.Description = Description;
+        this.ImageFilepath = ImageFilepath;
+    }
+}
